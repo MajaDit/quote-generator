372178f59bc737fe59faaa033a2556545337c7f9
MajaDit <marie.ditrtova@gmail.com>
2022-01-03 11:47:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get Quotes From Api
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+  } catch (error) {
+    //Catch Error Here
+  }
+}
