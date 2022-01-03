40f017b1e725a23814cc9fc8b60ce2288034a8c8
MajaDit <marie.ditrtova@gmail.com>
2022-01-03 15:39:48 +0000

    replaced place holders with dynamic quotes

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const author = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 //Show new quote
 function newQuote() {
   // pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  author.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From Api
