da4bb434d5dce1658b0f1f3aa3ab8bb4b8bb3e82
MajaDit <marie.ditrtova@gmail.com>
2022-01-03 12:15:27 +0000

    fetched the api using asynchronic funtion

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+let apiQuotes = [];
+
+//Show new quote
+function newQuote() {}
+
 // Get Quotes From Api
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
   } catch (error) {
     //Catch Error Here
   }
 }
+
+// On Load
+getQuotes();
